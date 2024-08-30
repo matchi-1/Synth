f0b7a848bd110ae61f3cc92120c03cef09a96a07
CoderTofu <vioneverforget@gmail.com>
2024-08-30 18:48:04 +0800

    completed most of the ui

--- src/components/article-page/sideBar.jsx
+++ src/components/article-page/sideBar.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
-import { IoIosCloseCircle, IoMdSend, IoIosArrowDown } from "react-icons/io";
+import { IoMdSend, IoMdClose, IoMdExpand } from "react-icons/io";
 import "../css/sideBar.css";
 
 const SideBar = ({ isSidebarOpen, toggleSidebar }) => {
   const [isDropdown, setDropdown] = useState(false);
   const [text, setText] = useState("");
   const [selectedOption, setSelectedOption] = useState("Highly Relevant");
+  const [noteType, setNoteType] = useState("HR");
   const textareaRef = useRef(null);
 
   const toggleDropdown = () => {
@@ -16,6 +17,14 @@ const SideBar = ({ isSidebarOpen, toggleSidebar }) => {
   const dropdownClick = (option) => {
     setSelectedOption(option);
     setDropdown(false);
+
+    if (option === "Highly Relevant") {
+      setNoteType("HR");
+    } else if (option === "Moderately Relevant") {
+      setNoteType("MR");
+    } else if (option === "Tangentially Relevant") {
+      setNoteType("TR");
+    }
   };
 
   useEffect(() => {
@@ -32,9 +41,18 @@ const SideBar = ({ isSidebarOpen, toggleSidebar }) => {
 
   return (
     <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
-      <button onClick={toggleSidebar}>
-        <IoIosCloseCircle />
-      </button>
+      <div className="toggle-btn-container">
+        <div>
+          <img
+            className="header-logo"
+            src="/assets/img/logo.png"
+            alt="logo.png"
+          />
+        </div>
+        <button onClick={toggleSidebar}>
+          <IoMdClose />
+        </button>
+      </div>
 
       <div className="legend-container">
         <div className="legend">
@@ -42,10 +60,10 @@ const SideBar = ({ isSidebarOpen, toggleSidebar }) => {
           <table>
             <tbody>
               <tr>
+                <td className="highly-relevant">Highly Relevant Text</td>
                 <td className="moderately-relevant">
                   Moderately Relevant Text
                 </td>
-                <td className="highly-relevant">Highly Relevant Text</td>
               </tr>
               <tr>
                 <td className="tangentially-relevant">
@@ -82,36 +100,58 @@ const SideBar = ({ isSidebarOpen, toggleSidebar }) => {
         <div className="notes-filter">
           <div className="styled-dropdown">
             <span>Include the following in your notes:</span>
-            <div className="dropdown">
-              <button className="dropdown-toggle" onClick={toggleDropdown}>
-                {selectedOption}
-              </button>
-              {isDropdown && (
-                <ul className="dropdown-menu">
-                  <li
-                    onClick={() => dropdownClick("Highly Relevant")}
-                    className="highly-relevant"
-                  >
-                    Highly Relevant
-                  </li>
-                  <li
-                    onClick={() => dropdownClick("Moderately Relevant")}
-                    className="moderately-relevant"
-                  >
-                    Moderately Relevant
-                  </li>
-                  <li
-                    onClick={() => dropdownClick("Tangentially Relevant")}
-                    className="tangentially-relevant"
-                  >
-                    Tangentially Relevant
-                  </li>
-                </ul>
-              )}
+            <div className="note-utility">
+              <div className="dropdown">
+                <button
+                  className={`dropdown-toggle ${noteType}`}
+                  onClick={toggleDropdown}
+                >
+                  {selectedOption}
+                </button>
+                {isDropdown && (
+                  <ul className="dropdown-menu">
+                    <li
+                      onClick={() => dropdownClick("Highly Relevant")}
+                      className="highly-relevant"
+                    >
+                      Highly Relevant
+                    </li>
+                    <li
+                      onClick={() => dropdownClick("Moderately Relevant")}
+                      className="moderately-relevant"
+                    >
+                      Moderately Relevant
+                    </li>
+                    <li
+                      onClick={() => dropdownClick("Tangentially Relevant")}
+                      className="tangentially-relevant"
+                    >
+                      Tangentially Relevant
+                    </li>
+                  </ul>
+                )}
+              </div>
+              <button className="add-note-btn">Add</button>
             </div>
           </div>
         </div>
-        <div className="notes"></div>
+        <div className="notes">
+          <h3>NOTES</h3>
+          <div className="note-list">
+            <div className="expand-notes-btn">
+              <IoMdExpand />
+            </div>
+            <div className={`note ${noteType}`}>
+              <span>{/** Insert [number] */}[2]</span>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Consequatur, ipsam voluptatum. Commodi sequi fuga maxime at
+                quasi. Nihil neque sequi odit. Excepturi libero praesentium
+                rerum laborum odio ex magni totam.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
