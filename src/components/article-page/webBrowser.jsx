759fae9f2a43d5716d95f7b369ca8c59149d061e
Rafael Torres <rafaelsebastian0908@gmail.com>
2024-08-30 19:36:56 +0800

    webbrowser impl

--- src/components/article-page/webBrowser.jsx
+++ src/components/article-page/webBrowser.jsx
@@ -1,7 +1,50 @@
-import React, { useState } from "react";
+  import React, { useRef, useState } from "react";
+  import { IoMdSearch } from "react-icons/io";
+  import axios from "axios";
+  import "../css/webBrowser.css";
 
-const WebBrowser = () => {
-  return <div></div>;
-};
+  const WebBrowser = () => {
+    const textRef = useRef();
+    const [list, setList] = useState([]);
+    
+    const handleChange = (event) => {
+      event.preventDefault();
+      textRef.current = event.target.value;
+      console.log(textRef.current);
+    }
 
-export default WebBrowser;
+    const handleSearch = async (event) => {
+        event.preventDefault();
+        const resp = await axios.get(`http://localhost:5000/?url=${textRef.current}`);
+        const data = resp.data;
+        setList([data]);
+    }
+    return (
+      <div>
+        <div className="frame">
+          <div className="url-container">
+            <div className="prompt">
+              <form className="prompt-box">
+              <textarea
+                ref={textRef}
+                onChange={handleChange}
+              />
+              <div className="prompt-button-container">
+                <button type="submit" onClick={handleSearch}>
+                  <IoMdSearch />
+                </button>
+              </div>
+              </form>
+            </div>
+          </div>
+        {
+          list.map((string) => {
+            return <div className="scrollable" dangerouslySetInnerHTML={{__html:string}}></div>
+          })
+        }
+        </div> 
+      </div>
+    );
+  };
+
+  export default WebBrowser;
